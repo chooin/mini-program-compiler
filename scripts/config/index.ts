f2923ae7503ad7b5b30991efbe950235e17e6e11
Devin.Zhu <chooin@126.com>
2021-05-13 23:16:29 +0800

    chore: esbuild

--- scripts/config/index.ts
+++ scripts/config/index.ts
@@ -1,17 +1,26 @@
 import dotenv from 'dotenv';
 
+interface ENV {
+  DEBUG: boolean;
+  [k: string]: any;
+}
+
 const config = dotenv.config({
   path: process.env.DOTENV,
 }).parsed;
 
-export const env = (() => {
+export const isProd: boolean =
+  typeof config.DEBUG === 'undefined' || JSON.parse(config.DEBUG) === false;
+
+export const env: ENV = (() => {
   const env = Object.create({});
   Object.keys(config).forEach((key) => {
-    env[`process.env.${key}`] =
-      typeof config[key] === 'string' ? `'${config[key]}'` : config[key];
+    if ('DEBUG' === key) {
+      env['process.env.DEBUG'] = !isProd;
+    } else {
+      env[`process.env.${key}`] =
+        typeof config[key] === 'string' ? `'${config[key]}'` : config[key];
+    }
   });
   return env;
 })();
-
-export const isProd =
-  typeof config.DEBUG !== 'undefined' && JSON.parse(config.DEBUG) === false;
