1b98aad3903225e36953a5f01b8a4302738ebcb9
Devin.Zhu <chooin@126.com>
2021-05-13 18:04:31 +0800

    chore: rollup

--- scripts/build/project-config-json.ts
+++ scripts/build/project-config-json.ts
@@ -2,17 +2,15 @@ import {logger, trace} from '../utils';
 import {json} from 'fast-files';
 
 export default (inputFile, outputFile) => {
-  (async () => {
-    logger.build(''.padEnd(6), inputFile);
-    json()
-      .readFile(inputFile)
-      .merge({
-        setting: {
-          enhance: true,
-          es6: true,
-        },
-      })
-      .saveFile(outputFile);
-    logger.create(trace.end(inputFile), `${inputFile} -> ${outputFile}`);
-  })();
+  logger.build(''.padEnd(6), inputFile);
+  json()
+    .readFile(inputFile)
+    .merge({
+      setting: {
+        enhance: true,
+        es6: true,
+      },
+    })
+    .saveFile(outputFile);
+  logger.create(trace.end(inputFile), `${inputFile} -> ${outputFile}`);
 };
--- scripts/build/scss.ts
+++ scripts/build/scss.ts
@@ -5,24 +5,24 @@ const px2rpx = require('postcss-pxtorpx-pro');
 import {logger, trace} from '../utils';
 
 export default (inputFile, outputFile) => {
-  (async () => {
-    logger.build(''.padEnd(6), inputFile);
-    const {css} = renderSync({
-      file: inputFile,
-    });
-    const {css: wxss} = await postcss()
-      .use(
-        px2rpx({
-          minPixelValue: 2,
-          transform: (x) => x,
-        }),
-      )
-      .process(css, {
-        map: false,
-        from: undefined,
+  logger.build(''.padEnd(6), inputFile);
+  const {css} = renderSync({
+    file: inputFile,
+  });
+  postcss()
+    .use(
+      px2rpx({
+        minPixelValue: 2,
+        transform: (x) => x,
+      }),
+    )
+    .process(css, {
+      map: false,
+      from: undefined,
+    })
+    .then(({css: wxss}) => {
+      writeFile(outputFile, wxss).then(() => {
+        logger.create(trace.end(inputFile), `${inputFile} -> ${outputFile}`);
       });
-    writeFile(outputFile, wxss).then(() => {
-      logger.create(trace.end(inputFile), `${inputFile} -> ${outputFile}`);
     });
-  })();
 };
