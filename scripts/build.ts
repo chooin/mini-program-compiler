dfeba3ab943974c8ea7d299b5c36a7e4b20620a7
Devin.Zhu <chooin@126.com>
2021-05-13 13:33:11 +0800

    chore: ~

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,126 +1,101 @@
-const chokidar = require('chokidar')
-const rollup = require('rollup')
-const postcss = require('postcss')
-const fse = require('fs-extra')
-const {renderSync} = require('dart-sass')
-const px2rpx = require('postcss-pxtorpx-pro')
-import {logger, trace, watchPaths, NODE_ENV} from './utils'
-import {json} from 'fast-files'
+const chokidar = require('chokidar');
+const rollup = require('rollup');
+const postcss = require('postcss');
+const fse = require('fs-extra');
+const {renderSync} = require('dart-sass');
+const px2rpx = require('postcss-pxtorpx-pro');
+import {logger, trace, watchPaths, NODE_ENV} from './utils';
+import {json} from 'fast-files';
 
 const build = async (file) => {
-  trace.start(file)
+  trace.start(file);
   if (/\.ts$/.test(file)) {
-    const outputFile = file
-      .replace(/^src/, 'dist')
-      .replace(/\.ts$/, '.js')
-    logger.build(
-      file.replace(/^src\//, ''),
-    )
+    const outputFile = file.replace(/^src/, 'dist').replace(/\.ts$/, '.js');
+    logger.build(file.replace(/^src\//, ''));
     const bundle = await rollup.rollup({
-      ...(require(`./rollup.config.${NODE_ENV}`)),
+      ...require(`./rollup.config.${NODE_ENV}`),
       input: file,
-    })
+    });
     await bundle.write({
       file: outputFile,
       exports: 'named',
       format: 'cjs',
-    })
-    logger.create(
-      outputFile.replace(/^dist\//, ''),
-      trace.end(file)
-    )
-    return
+    });
+    logger.create(outputFile.replace(/^dist\//, ''), trace.end(file));
+    return;
   }
   if (/\.scss$/.test(file)) {
-    logger.build(
-      file.replace(/^src\//, ''),
-    )
-    const outputFile = file
-      .replace(/^src/, 'dist')
-      .replace(/\.scss$/, '.wxss')
-    const outputDir = outputFile.replace(/\/[\w-_]+.wxss/, '')
+    logger.build(file.replace(/^src\//, ''));
+    const outputFile = file.replace(/^src/, 'dist').replace(/\.scss$/, '.wxss');
+    const outputDir = outputFile.replace(/\/[\w-_]+.wxss/, '');
     const {css} = renderSync({
       file,
-    })
+    });
     if (!fse.existsSync(outputDir)) {
-      await fse.mkdirp(outputDir)
+      await fse.mkdirp(outputDir);
     }
     const {css: wxss} = await postcss()
       .use(
         px2rpx({
           minPixelValue: 2,
-          transform: (x) => x
-        })
+          transform: (x) => x,
+        }),
       )
       .process(css, {
         map: false,
-        from: undefined
-      })
-    await fse.writeFile(outputFile, wxss, () => {})
-    logger.create(
-      outputFile.replace(/^dist\//, ''),
-      trace.end(file),
-    )
-    return
+        from: undefined,
+      });
+    await fse.writeFile(outputFile, wxss, () => {});
+    logger.create(outputFile.replace(/^dist\//, ''), trace.end(file));
+    return;
   }
   if (/project.config.json$/.test(file)) {
-    const outputFile = file.replace(/^src/, 'dist')
-    json
+    const outputFile = file.replace(/^src/, 'dist');
+    json()
       .readFile(file)
       .merge({
         setting: {
           enhance: true,
           es6: true,
-        }
+        },
       })
-      .saveFile(outputFile)
-    logger.create(
-      file.replace(/^src\//, ''),
-      trace.end(file),
-    )
-    return
+      .saveFile(outputFile);
+    logger.create(file.replace(/^src\//, ''), trace.end(file));
+    return;
   }
 
-  await fse.copy(file, file.replace(/^src/, 'dist'))
-  logger.copy(
-    file.replace(/^src\//, ''),
-    trace.end(file),
-  )
-}
+  await fse.copy(file, file.replace(/^src/, 'dist'));
+  logger.copy(file.replace(/^src\//, ''), trace.end(file));
+};
 
 const run = () => {
-  return Promise.resolve()
-}
+  return Promise.resolve();
+};
 
 const removeDist = () => {
-  return fse.remove('dist')
-}
+  return fse.remove('dist');
+};
 
 (() => {
   run()
     .then(() => {
       if (NODE_ENV === 'prod') {
-        return removeDist()
+        return removeDist();
       }
     })
     .then(() => {
-      const watcher = chokidar
-        .watch(watchPaths, {
-          ignored: [
-            '**/.DS_Store',
-            '**/.gitkeep'
-          ],
-        })
+      const watcher = chokidar.watch(watchPaths, {
+        ignored: ['**/.DS_Store', '**/.gitkeep'],
+      });
 
-      watcher.on('add',(file) => {
-        return build(file)
-      })
+      watcher.on('add', (file) => {
+        return build(file);
+      });
 
-      watcher.on('change',(file) => {
-        return build(file)
-      })
+      watcher.on('change', (file) => {
+        return build(file);
+      });
 
-      watcher.on('ready', () => {
-      })
-  })
-})()
+      watcher.on('ready', () => {});
+    });
+})();
--- scripts/utils/watch-paths.ts
+++ scripts/utils/watch-paths.ts
@@ -1,14 +1,18 @@
-import {json} from 'fast-files'
-import path from 'path'
+import {json} from 'fast-files';
+import path from 'path';
 
 export default (() => {
-  const config = json.readFile(path.resolve(__dirname, '../../src/project.config.json'))
-  const compileType = config.get('compileType')
-  const miniprogramRoot = config.get('miniprogramRoot')
-  const pluginRoot = config.get('pluginRoot')
+  const config = json().readFile(
+    path.resolve(__dirname, '../../src/project.config.json'),
+  );
+  const compileType = config.get('compileType');
+  const miniprogramRoot = config.get('miniprogramRoot');
+  const pluginRoot = config.get('pluginRoot');
   if (compileType === 'plugin') {
-    return ['project.config.json', miniprogramRoot, pluginRoot].map((path) => `src/${path}`)
+    return ['project.config.json', miniprogramRoot, pluginRoot].map(
+      (path) => `src/${path}`,
+    );
   } else {
-    return [miniprogramRoot, pluginRoot].map((path) => `src/${path}`)
+    return [miniprogramRoot, pluginRoot].map((path) => `src/${path}`);
   }
-})()
+})();
