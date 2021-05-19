4d0e7368204b3ba05c4b3075cde0c874692fba3d
Devin.Zhu <chooin@126.com>
2021-05-19 13:23:56 +0800

    chore: remove rollup

--- scripts/build/typescript.ts
+++ scripts/build/typescript.ts
@@ -1,5 +1,5 @@
 import {existsSync, mkdirpSync} from 'fs-extra';
-import {build} from 'esbuild';
+import {buildSync} from 'esbuild';
 
 import {file, logger, trace} from '../utils';
 import {isProd, env as define} from '../config';
@@ -10,16 +10,14 @@ export default (path) => {
   if (!existsSync(outputDir)) {
     mkdirpSync(outputDir);
   }
-  (async () => {
-    logger.build(''.padEnd(6), inputFile);
-    await build({
-      entryPoints: [inputFile],
-      outfile: outputFile,
-      minify: isProd,
-      treeShaking: isProd ? isProd : 'ignore-annotations',
-      bundle: false,
-      define,
-    })
-    logger.create(trace.end(inputFile), `${inputFile} -> ${outputFile}`);
-  })();
+  logger.build(''.padEnd(6), inputFile);
+  buildSync({
+    entryPoints: [inputFile],
+    outfile: outputFile,
+    minify: isProd,
+    treeShaking: isProd ? isProd : 'ignore-annotations',
+    bundle: false,
+    define,
+  })
+  logger.create(trace.end(inputFile), `${inputFile} -> ${outputFile}`);
 };
