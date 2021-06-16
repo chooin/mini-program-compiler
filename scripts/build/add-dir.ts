2f96303c692aed46bc2a23ba0f2d9f44b245f8fc
Devin.Zhu <chooin@126.com>
2021-06-16 18:29:26 +0800

    fix: add dir

--- scripts/build/add-dir.ts
+++ scripts/build/add-dir.ts
@@ -2,16 +2,15 @@ import {existsSync, mkdirp, mkdirpSync} from 'fs-extra';
 import {file, logger, trace} from '../utils';
 
 /**
- * 创建目录
+ * 目录不存在，则创建目录
  * @param {string} path
  */
 export default (path: string) => {
-  trace.start(path);
   const {inputDir, outputDir} = file.path(path);
   if (!existsSync(outputDir)) {
-    mkdirpSync(outputDir);
+    trace.start(inputDir);
+    mkdirp(outputDir).then(() => {
+      logger.create(trace.end(inputDir), `${inputDir} -> ${outputDir}`);
+    });
   }
-  mkdirp(outputDir).then(() => {
-    logger.create(trace.end(inputDir), `${inputDir} -> ${outputDir}`);
-  });
 };
